c14f3353d03080ce34e9f84828a69624a1c0cd98
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2025-02-28 14:44:42 +0530

    Add project files.

---
+++ LawProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LawProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
